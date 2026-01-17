265fa950a979433942f7cd8183d014e81720d310
AlejandroMena <2024ds113@utez.edu.mx>
2026-01-16 21:28:18 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -3,5 +3,8 @@ public class Main{
         System.out.println("Semana 2");
         System.out.println("Version base");
 
+        System.out.println("2 + 2 = " + (2 + 2));
+        System.out.println("5 * 3 = " + (5 * 3));
+
     }
 }
